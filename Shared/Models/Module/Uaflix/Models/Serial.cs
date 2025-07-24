92b033be184ec8cf9e739f6fcfcf285276520261
lilllilililllililil <ljiiiliililllllilijl@gmail.com>
2025-07-24 15:41:27 +0200

    Add files via upload

---
+++ Shared/Models/Module/Uaflix/Models/Serial.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Uaflix.Models.UaFlix
+{
+    public class Serial
+    {
+        public string id { get; set; }
+
+        public List<(string link, string quality)> links { get; set; }
+    }
+}
---
+++ Shared/Models/Module/Uaflix/Models/Voice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Uaflix.Models.UaFlix
+{
+    public class Voice
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public List<Serial> episodes { get; set; }
+    }
+}
