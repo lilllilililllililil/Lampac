b7649e86c8ed61f0f4fc40fa4516c01314e8d58e
lilllilililllililil <ljiiiliililllllilijl@gmail.com>
2025-07-24 15:39:40 +0200

    Add files via upload

---
+++ Shared/Models/Module/Uaflix/ModInit.cs
@@ -0,0 +1,24 @@
+﻿using Lampac;
+using Lampac.Models.LITE;
+
+namespace Uaflix
+{
+    public class ModInit
+    {
+        public static OnlinesSettings UaFlix;
+
+        /// <summary>
+        /// модуль загружен
+        /// </summary>
+        public static void loaded()
+        {
+            UaFlix = new OnlinesSettings("UaFlix", "uafix.net", streamproxy: true)
+            {
+                displayname = "UaFlix"
+            };
+
+            // Выводить "уточнить поиск"
+            AppInit.conf.online.with_search.Add("uaflix");
+        }
+    }
+}
